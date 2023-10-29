15449c2add01f053d0fabfe87193b1a783503201
Johnny <delga300@hotmail.com>
2023-10-28 23:00:35 -0300

    API Node com Postgres

---
+++ server.js
@@ -0,0 +1,57 @@
+import { fastify } from 'fastify'
+import { DatabasePostgress } from './database-postgres.js'
+
+const server = fastify()
+
+const database = new DatabasePostgress()
+
+server.post('/videos', async (resquest, response) => {
+
+    const {title, description, duration} = resquest.body
+
+
+    await database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return response.status(201).send()
+})
+
+server.get('/videos', async (resquest) => {
+    const search = resquest.query.search
+
+    console.log(search)
+
+    const videos = await database.list(search)
+
+    console.log(videos)
+
+    return videos
+})
+
+server.put('/videos/:id', async (resquest) => {
+    const videoId = resquest.params.id
+    const {title, description, duration} = resquest.body
+
+    await database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return response.status(204).send()
+})
+
+server.delete('/videos/:id', async (resquest, response) => {
+    const videoId = resquest.params.id
+
+    await database.delete(videoId)
+
+    return response.status(204).send()
+})
+
+server.listen({
+    port: process.env.PORT ?? 3333,
+})
